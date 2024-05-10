d836aab7be981e9c37cd7670c87c2775eb43be75
Jetezh 123 <vegasetiawan16@gmail.com>
2024-05-10 15:53:07 +0000

    solution change

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
+app.use(express.urlencoded({extended: true}));
+
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
@@ -19,18 +21,46 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.post("/api/shorturl", (req, res) => {
-  const requestedUrl = req.originalUrl;
+const originalUrls = [];
+const shortnerUrls = [];
 
-  res.json({
-    original_url: requestedUrl,
-    short_url:
+app.post("/api/shorturl", (req, res) => {
+  const url = req.body.url;
+  const findIndex = originalUrls.indexOf(url);
+
+  if(!url.includes("https://") && !url.includes("http://")){
+    res.json({error: "invalid url"});
+  }
+
+  if(findIndex < 0) {
+    originalUrls.push(url);
+    shortnerUrls.push(shortnerUrls.length)
+
+    return res.json({
+      original_url: url,
+      short_url: shortnerUrls.length - 1
+    });
+  }
+
+  return res.json({
+    original_url: url,
+    short_url: shortnerUrls[findIndex]
   })
+
 })
 
-app.get('api/shorturl/{short_url}?', (req, res) => {
-  res.
-}) 
+app.get("/api/shorturl/:shorturl", (req, res) => {
+  const p_shorturl = req.params.shorturl;
+  const findIndex = shortnerUrls.indexOf(p_shorturl);
+
+  if(findIndex < 0) {
+    res.json({
+      error: "No short URL found for the given input"
+    })
+  }
+
+  res.redirect(originalUrls[findIndex]);
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
